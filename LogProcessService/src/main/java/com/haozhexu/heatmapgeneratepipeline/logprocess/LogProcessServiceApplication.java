b9c0d889f38a007a8b5eda8b580b9b8aeaa9bf04
Haozhe Xu <jamesxuhaozhe@gmail.com>
2018-11-28 18:04:41 +0800

    Create LogProcessService whose responsibility is to process the request from the outside application and flush them into log directory

---
+++ LogProcessService/src/main/java/com/haozhexu/heatmapgeneratepipeline/logprocess/LogProcessServiceApplication.java
@@ -0,0 +1,12 @@
+package com.haozhexu.heatmapgeneratepipeline.logprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogProcessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogProcessServiceApplication.class, args);
+	}
+}
---
+++ LogProcessService/src/test/java/com/haozhexu/heatmapgeneratepipeline/logprocess/LogProcessServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.haozhexu.heatmapgeneratepipeline.logprocess;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LogProcessServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
