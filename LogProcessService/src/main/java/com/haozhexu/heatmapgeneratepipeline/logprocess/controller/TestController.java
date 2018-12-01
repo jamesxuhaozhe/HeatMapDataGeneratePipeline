09661bcb89ff3db71774dcd870394701d1f71594
Haozhe Xu <jamesxuhaozhe@gmail.com>
2018-12-01 22:24:15 +0800

    added log4j2 config and flume client dependency. I need to cleen it up them later

--- LogProcessService/src/main/java/com/haozhexu/heatmapgeneratepipeline/logprocess/controller/TestController.java
+++ LogProcessService/src/main/java/com/haozhexu/heatmapgeneratepipeline/logprocess/controller/TestController.java
@@ -1,19 +1,38 @@
 package com.haozhexu.heatmapgeneratepipeline.logprocess.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @PostMapping(value = "/test")
     public String test(String input) {
+        logger.info("input:" + input);
         return input;
     }
 
-    @GetMapping(value = "/tt")
+/*    @GetMapping(value = "/tt")
     public String testGet() {
         return "test";
     }
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            i++;
+
+            logger.info("input:" + i);
+        }
+
+    }*/
 }
